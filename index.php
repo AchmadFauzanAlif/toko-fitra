37ab0aa6c6dc1fe291b91f053c009bb46aba6ad2
Achmad Fauzan Alif Fitrah <fznfitrah@gmail.com>
2024-12-29 20:01:53 +0700

    first commit

---
+++ index.php
@@ -0,0 +1,88 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Toko Fitra</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css">
+    <link rel="stylesheet" href= "css/style.css">
+</head>
+
+<body>
+    <nav class="navbar navbar-expand-lg bg-warning">
+        <div class="container ">
+            <img src="" alt="Logo Toko">
+            Fitra Cell
+            <div class="collapse navbar-collapse" >
+                <ul class="navbar-nav mx-auto">
+                    <li class="navbar-item">
+                        <span>Hallo Fitrah, Selamat Datang di App Kasir</span>
+                    </li class="navbar-item">
+                </ul>
+
+                <ul class="navbar-nav ml-auto">
+                    <li class="nav-item dropdown" data-bs-toggle="dropdown" aria-expanded="false">
+                        <button class="btn btn-dark dropdown-toggle">
+                            <span>Menu</span>
+                        </button>
+                        <ul class="dropdown-menu">
+                            <li class="dropdown-item"><a href="" class="nav-link">Profile</a></li>
+                            <li class="dropdown-item bg-danger"><a href="" class="nav-link">Logout</a></li>
+                        </ul>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+
+    
+    <div class="content">
+        <div class="col-lg-3 col-sm-6 menu" >
+            <div class="card-box bg-primary">
+                <div class="inner">
+                    <h3>Transaksi</h3>
+                    <p class=""><a href="" class="text-light link-underline link-underline-opacity-0">Menu Transaksi</a></p>
+                </div>
+                <div class="icon">
+                    <i class="bi bi-bag-check-fill"></i>
+                </div>
+            </div>
+        </div>
+        <div class="col-lg-3 col-sm-6 menu">
+            <div class="card-box bg-primary">
+                <div>
+                    <h3>Barang</h3>
+                    <p class=""><a href="" class="text-light link-underline link-underline-opacity-0">Menu Barang</a></p>
+                </div>
+                <div class="icon">
+                    <i class="bi bi-archive-fill"></i>
+                </div>
+            </div>
+        </div>
+        <div class="col-lg-3 col-sm-6 menu">
+            <div class="card-box bg-primary">
+                <div>
+                    <h3>Laporan</h3>
+                    <p class=""><a href="" class="text-light link-underline link-underline-opacity-0">Menu Laporan</a></p>
+                </div>
+                <div class="icon">
+                    <i class="bi bi-bar-chart-fill"></i>
+                </div>
+            </div>
+        </div>
+    </div>
+
+
+<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js" integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.min.js" integrity="sha384-0pUGZvbkm6XF6gxjEnlmuGrJXVbNuzT9qBBavbLwCsOGabYfZo0T0to5eqruptLy" crossorigin="anonymous"></script>
+</body>
+</html>
+echo "# toko-fitra" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/AchmadFauzanAlif/toko-fitra.git
+git push -u origin main
